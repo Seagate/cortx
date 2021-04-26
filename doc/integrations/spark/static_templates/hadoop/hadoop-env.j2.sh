9586b2eaeeeb615257c5032c5f2faf319af98532
Stephen M <stephen@pop-os.localdomain>
2021-04-26 22:56:46 +0800

    adds initial commit for spark integration for the devpost/cortx hackathon.
    This commit includes a link to a video (youtube), installation documentation, a bit of background on Spark/Hadoop and ansible script for easy installation of the integration.
    
    This commit was tested using the CORTX OVA image using VmWare Workstation for the CORTX provider, and an Ubuntu 20.04 image running under VmWare Workstation for the integration.
    
    Signed-off-by: Stephen M <stephen@motts.id.au>

---
+++ doc/integrations/spark/static_templates/hadoop/hadoop-env.j2.sh
@@ -0,0 +1,4 @@
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
+export AWS_ACCESS_KEY_ID={{s3_access_key}}
+export AWS_SECRET_ACCESS_KEY={{s3_secret_key}}
+export HADOOP_OPTIONAL_TOOLS=hadoop-aws
