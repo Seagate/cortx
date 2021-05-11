c8b7d0784f362e7cce4bd6a58e25d9f88019e779
mikoaro <37387347+mikoaro@users.noreply.github.com>
2021-05-10 19:48:58 -0500

    Added edgehealth.md and source codes (#982)
    * Create edgehealth.md
    
    intial commit
    
    * doing it live
    
    * doing it live
    
    * make it better
    
    * make it better
    
    * make it better
    
    * make it better
    
    * make it better
    
    * make it better
    
    * Delete .DS_Store
    
    Signed-off-by: John Bent <john.bent@seagate.com>
    
    Co-authored-by: Ifamimiko <datastarx@gmail.com>
    Co-authored-by: John Bent <john.bent@seagate.com>

---
+++ doc/integrations/edgehealth/edgehealth-chatbot/src/components/pages/Landing.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Landing=()=>{
+    return (
+        <div>
+            <h1 style={{textAlign:'center'}}>EdgeHealth Bot</h1>
+        </div>
+    )
+}
+export default Landing;
---
+++ doc/integrations/edgehealth/edgehealth-loggerapi/config.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+
+const {Pool} = require('pg')
+const isProduction = process.env.NODE_ENV === 'production'
+
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
+
+const pool = new Pool({
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  ssl: { rejectUnauthorized: false },
+})
+
+module.exports = {pool}
