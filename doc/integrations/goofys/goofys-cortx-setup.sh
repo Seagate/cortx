c65e84ca2c3559b3ce3c04b01552914208811374
c-l-j-y <88305571+c-l-j-y@users.noreply.github.com>
2021-08-02 17:10:22 +0800

    Update goofys-cortx-setup.sh
    Signed-off-by: c-l-j-y <88305571+c-l-j-y@users.noreply.github.com>

--- doc/integrations/goofys/goofys-cortx-setup.sh
+++ doc/integrations/goofys/goofys-cortx-setup.sh
@@ -7,19 +7,19 @@ aws configure set aws_access_key_id $3 --profile default
 aws configure set aws_secret_access_key $4 --profile default
 aws configure set region us-east-1 --profile default
 
-mkdir -p /root/go/src/github.com/kahing/
-cd /root/go/src/github.com/kahing/
+mkdir -p $HOME/go/src/github.com/kahing/
+cd $HOME/go/src/github.com/kahing/
 git clone https://github.com/kahing/goofys.git
 
-export GOPATH=/root/go
-export GOOFYS_HOME=/root/go/src/github.com/kahing/goofys/
+export GOPATH=$HOME/go
+export GOOFYS_HOME=$HOME/go/src/github.com/kahing/goofys/
 
-cd /root/go/src/github.com/kahing/goofys
+cd $HOME/go/src/github.com/kahing/goofys
 git submodule init
 git submodule update
 
-go install /root/go/src/github.com/kahing/goofys
-PATH=$PATH:/root/go/bin; export PATH
+go install $HOME/go/src/github.com/kahing/goofys
+PATH=$PATH:$HOME/go/bin; export PATH
 
-mkdir -p /root/shared
-goofys --endpoint http://$1 -o allow_other $2 /root/shared
+mkdir -p $HOME/shared
+goofys --endpoint http://$1 $2 $HOME/shared
