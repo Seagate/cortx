2087d7a5efa6b000b4c9e7afdba29e165c583d1b
Mukul Malhotra <mukul.malhotra@seagate.com>
2021-09-22 08:15:38 -0600

    presetup script
    Signed-off-by: Mukul Malhotra <mukul.malhotra@seagate.com>

---
+++ doc/community-build/presetup.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+SCRIPT_PATH=/mnt/cortx/scripts
+sed -i '38,84d' /etc/nginx/nginx.conf
+
+###########################################################################
+## Append the locally hosted packages directory in /etc/nginx/nginx.conf ##
+###########################################################################
+
+cat <<EOF>>/etc/nginx/nginx.conf
+server {
+   listen *:80;
+   server_name 127.0.0.1 ${LOCAL_IP};
+   location /0 {
+   root /var/artifacts;
+   autoindex on;
+             }
+         }
+    }
+EOF
+
+#######################################################
+## Run the following commands to start nginx service ##
+#######################################################
+
+systemctl start nginx
+systemctl enable nginx
+
+######################################################
+## Run the following commands to allow HTTP traffic ##
+######################################################
+
+firewall-cmd --permanent --zone=public --add-service=http
+firewall-cmd --reload
+
+################################
+## Run the following commands ##
+################################
+
+
+mv /var/artifacts/0/install-2.0.0-0.sh $SCRIPT_PATH/install.sh
+sed -i '/udx-discovery/d;/uds-pyi/d' $SCRIPT_PATH/install.sh
+sed -i 's/trusted-host: cortx-storage.colo.seagate.com/trusted-host: '$LOCAL_IP'/' $SCRIPT_PATH/install.sh
+sed -i 's#cortx-storage.colo.seagate.com|file://#cortx-storage.colo.seagate.com|baseurl=file:///#' $SCRIPT_PATH/install.sh
+sed -i '269s#yum-config-manager --add-repo "${repo}/3rd_party/" >> "${LOG_FILE}"#yum-config-manager --nogpgcheck --add-repo "${repo}/3rd_party/" >> "${LOG_FILE}"#' $SCRIPT_PATH/install.sh
+yum update -y
