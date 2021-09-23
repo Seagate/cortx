5a7595e2bb87731cae1ac0b2926bd3375a552339
Mukul Malhotra <mukul.malhotra@seagate.com>
2021-09-23 07:36:25 -0600

    presetup script
    Signed-off-by: Mukul Malhotra <mukul.malhotra@seagate.com>

--- doc/community-build/presetup.sh
+++ doc/community-build/presetup.sh
@@ -1,5 +1,7 @@
-#!/bin/bash
+!#/bin/bash
 
+export SCRIPT_PATH=/mnt/cortx/scripts
+yum install epel-release -y
 sed -i '38,84d' /etc/nginx/nginx.conf
 
 ###########################################################################
@@ -36,11 +38,11 @@ firewall-cmd --reload
 ## Run the following commands ##
 ################################
 
-export SCRIPT_PATH=/mnt/cortx/scripts
-yum install epel-release -y && \
-mv /var/artifacts/0/install-2.0.0-0.sh $SCRIPT_PATH/install.sh && \
-cd $SCRIPT_PATH && curl -O https://raw.githubusercontent.com/Seagate/cortx-prvsnr/main/srv/components/provisioner/scripts/install.sh && \
-sed -i '/udx-discovery/d;/uds-pyi/d' $SCRIPT_PATH/install.sh && \
-sed -i 's/trusted-host: cortx-storage.colo.seagate.com/trusted-host: '$LOCAL_IP'/' $SCRIPT_PATH/install.sh && \
+
+
+mv /var/artifacts/0/install-2.0.0-0.sh $SCRIPT_PATH/install.sh
+cd $SCRIPT_PATH && curl -O https://raw.githubusercontent.com/Seagate/cortx-prvsnr/main/srv/components/provisioner/scripts/install.sh
+sed -i '/udx-discovery/d;/uds-pyi/d' $SCRIPT_PATH/install.sh
+sed -i 's/trusted-host: cortx-storage.colo.seagate.com/trusted-host: '$LOCAL_IP'/' $SCRIPT_PATH/install.sh
 sed -i 's#cortx-storage.colo.seagate.com|file://#cortx-storage.colo.seagate.com|baseurl=file:///#' $SCRIPT_PATH/install.sh
 sed -i '269s#yum-config-manager --add-repo "${repo}/3rd_party/" >> "${LOG_FILE}"#yum-config-manager --nogpgcheck --add-repo "${repo}/3rd_party/" >> "${LOG_FILE}"#' $SCRIPT_PATH/install.sh
