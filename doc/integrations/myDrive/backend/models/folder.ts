02467f33ab86464d2864d6c1cca153d7fd32ec85
Bari Arviv <bari.arviv@seagate.com>
2021-09-26 14:02:24 +0300

    Add the missing files for the integration
    Signed-off-by: Bari Arviv <bari.arviv@seagate.com>

---
+++ doc/integrations/myDrive/backend/models/folder.ts
@@ -0,0 +1,45 @@
+import mongoose, {Document} from "mongoose";
+
+const folderSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        required: true,
+    },
+
+    parent: {
+        type: String,
+        required: true,
+    },
+
+    owner: {
+        type: String,
+        required: true
+    },
+
+    parentList: {
+        type: Array,
+        required:true
+    },
+    personalFolder: Boolean
+
+}, {
+    timestamps: true
+})
+
+// @ts-ignore
+export interface FolderInterface extends Document {
+    name: string,
+    parent: string,
+    owner: string,
+    createdAt: Date,
+    updatedAt: Date,
+    parentList: string[],
+    _doc?: any,
+    personalFolder?: boolean
+}
+
+// @ts-ignore
+const Folder = mongoose.model<FolderInterface>("Folder", folderSchema);
+
+export default Folder;
