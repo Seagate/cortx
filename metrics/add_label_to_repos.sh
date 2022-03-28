b6602ba69308bd731d6829f1426f169f1cd0b3d2
Justin Woo <justin.woo@seagate.com>
2022-03-28 11:56:20 -0700

    Update add_label_to_repos.sh (#1425)
    * Update add_label_to_repos.sh
    
    Added new Triage labels
    
    Signed-off-by: Justin Woo <justin.woo@seagate.com>
    
    * Update add_label_to_repos.sh
    
    Updating to $GH_USER and removing John's name.
    Signed-off-by: Justin Woo <justin.woo@seagate.com>
    
    * Update add_repos_to_team.sh
    
    Updated this to $GH_USER and removed John's name.
    Signed-off-by: Justin Woo <justin.woo@seagate.com>
    
    Co-authored-by: John Bent <john.bent@seagate.com>

--- metrics/add_label_to_repos.sh
+++ metrics/add_label_to_repos.sh
@@ -18,5 +18,13 @@ echo $PYTHON
 for repo in `python3 -c "$PYTHON"`
 do
   echo $repo
-  curl -s -u "johnbent:$GH_OATH" -X POST "https://api.github.com/repos/$repo/labels" -d '{"name":"Status: Confirm Fix", "color":"0E8A16"}'
+  curl -s -u "$GH_USER:$GH_OATH" -X POST "https://api.github.com/repos/$repo/labels" -d '{"name":"Status: Backlog", "color":"DFFF00"}'
+  curl -s -u "$GH_USER:$GH_OATH" -X POST "https://api.github.com/repos/$repo/labels" -d '{"name":"Status: Fix Scheduled", "color":"FFBF00"}'
+  curl -s -u "$GH_USER:$GH_OATH" -X POST "https://api.github.com/repos/$repo/labels" -d '{"name":"Status: L1 Triage", "color":"FF7F50"}'
+  curl -s -u "$GH_USER:$GH_OATH" -X POST "https://api.github.com/repos/$repo/labels" -d '{"name":"Status: L2 Triage", "color":"DE3163"}'
+  curl -s -u "$GH_USER:$GH_OATH" -X POST "https://api.github.com/repos/$repo/labels" -d '{"name":"Status: More Info Needed", "color":"9FE2BF"}'
+  curl -s -u "$GH_USER:$GH_OATH" -X POST "https://api.github.com/repos/$repo/labels" -d '{"name":"Status: Resolved", "color":"40E0D0"}'
+  curl -s -u "$GH_USER:$GH_OATH" -X POST "https://api.github.com/repos/$repo/labels" -d '{"name":"Status: Will NOT Fix", "color":"6495ED"}'
+  curl -s -u "$GH_USER:$GH_OATH" -X POST "https://api.github.com/repos/$repo/labels" -d '{"name":"Status: DevAd", "color":"CCCCFF"}'
+  curl -s -u "$GH_USER:$GH_OATH" -X POST "https://api.github.com/repos/$repo/labels" -d '{"name":"Status: DevTeam", "color":"0E8A16"}'
 done
