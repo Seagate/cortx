518e2db745bc43f0bf391714ae8cf04cbf189771
John Bent <john.bent@seagate.com>
2022-04-04 11:49:53 -0600

    Changed which label is being updated

--- metrics/add_label_to_repos.sh
+++ metrics/add_label_to_repos.sh
@@ -2,8 +2,8 @@
 
 # ugh can't figure out how to use the variable
 # have to hard code in the below curl line
-LABEL="Status: Confirm Fix"
-COLOR="0E8A16"
+#LABEL="SODACODE2022"
+#COLOR="37E1E6"
 
 PYTHON=$(cat <<-END
 import cortx_community as cc
@@ -18,5 +18,5 @@ echo $PYTHON
 for repo in `python3 -c "$PYTHON"`
 do
   echo $repo
-  curl -s -u "johnbent:$GH_OATH" -X POST "https://api.github.com/repos/$repo/labels" -d '{"name":"Status: Confirm Fix", "color":"0E8A16"}'
+  curl -s -u "johnbent:$GH_OATH" -X POST "https://api.github.com/repos/$repo/labels" -d '{"name":"SODALOW", "color":"0E8A16"}'
 done
