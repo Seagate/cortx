6a90853c3791d1fe6c82d40596f784896ae372fd
Patrick Hession <patrick.hession@seagate.com>
2022-08-31 16:10:56 +0100

    Codacy "Expressions don't expand in single quotes"
    Signed-off-by: Patrick Hession <patrick.hession@seagate.com>

--- doc/community-build/cloud/AWS/setup.sh
+++ doc/community-build/cloud/AWS/setup.sh
@@ -30,8 +30,8 @@ for nic in eth1 eth2
 do
    MAC=$(ifconfig $nic | grep -o -E '([[:xdigit:]]{2}:){5}[[:xdigit:]]{2}')
    cp /etc/sysconfig/network-scripts/ifcfg-eth0 /etc/sysconfig/network-scripts/ifcfg-$nic
-   sed -i 's/eth0/$nic/g' /etc/sysconfig/network-scripts/ifcfg-$nic
-   sed -i -e '/HWADDR/d' /etc/sysconfig/network-scripts/ifcfg-$nic
+   sed -i "s/eth0/$nic/g" /etc/sysconfig/network-scripts/ifcfg-$nic
+   sed -i -e "/HWADDR/d" /etc/sysconfig/network-scripts/ifcfg-$nic
    sed -i "5 a HWADDR=$MAC" /etc/sysconfig/network-scripts/ifcfg-$nic
    echo -e "SUBSYSTEM==\"net\", ACTION==\"add\", DRIVERS==\"?*\", ATTR{address}==\"$MAC\", NAME=\"$nic\"" >> /etc/udev/rules.d/70-persistent-net.rules
 done
